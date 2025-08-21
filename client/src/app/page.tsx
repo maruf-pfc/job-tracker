2dac343daa53da68ca3e86c94c6f54d6366e802c
maruf-pfc <mdmarufsarker.mms@gmail.com>
2025-08-21 14:01:39 +0600

    chore: project initialized

---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="text-5xl text-white bg-indigo-500 rounded-lg p-4">
+          Hello World!
+        </h1>
+      </div>
+    </>
+  );
+}
