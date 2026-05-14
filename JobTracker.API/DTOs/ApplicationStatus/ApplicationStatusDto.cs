661b786240d0c957f3eb62d33167360e7df558c9
maruf-pfc <mdmarufsarker.mms@gmail.com>
2026-05-14 16:59:18 +0600

    fix: Application status DTO typo

--- JobTracker.API/DTOs/ApplicationStatus/ApplicationStatusDto.cs
+++ JobTracker.API/DTOs/ApplicationStatus/ApplicationStatusDto.cs
@@ -3,6 +3,5 @@ namespace JobTracker.API.DTOs.ApplicationStatus;
 public class ApplicationStatusDto
 {
     public Guid Id { get; set; }
-
     public string Name { get; set; } = string.Empty;
-}b
+}
